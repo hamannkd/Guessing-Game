7fc8b5b31cc60a221845b49e409dc7cefc541625
hamannkd <84426642+hamannkd@users.noreply.github.com>
2022-11-21 14:21:49 -0800

    Update GuessingGame.java
    Added the beginning text

--- GuessingGame.java
+++ GuessingGame.java
@@ -1,5 +1,9 @@
 public class GuessingGame {
     public static void main(String[] args) {
+
+        System.out.println("\nHello and welcome to a Guessing Game!");
+        System.out.println("Please guess a number between 1 and 10. You have three guesses");
+        
         
     }
 }
